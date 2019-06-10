459b3efcb80b03426b59281c8b311ba6a0486c42
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 19:49:46 +0530

    Converted to js

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+function doSomething()
+{
+    for(let i=0;i<19;i++)
+    {
+        console.log('Finally: '+i);
+    }
+
+ //   console.log('Finally: ');
+
+}
+
+doSomething();
+
+
+
+/*function func(message)
+{
+    console.log(message);
+}
+
+var msg="Hello World";
+
+func(msg);*/ 
