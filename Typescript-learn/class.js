5e4c42aca7bd9faaa9c1a03aff76addab65f1b07
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 21:46:17 +0530

    Implemented Getters

--- Typescript-learn/class.js
+++ Typescript-learn/class.js
@@ -5,17 +5,20 @@ var Point = /** @class */ (function () {
         this.x = x;
         this.y = y;
         //  this.x=x;
-        // this.y=y;
+        //   sthis.y=y;
     }
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ', Y:' + this.y);
     };
-    Point.prototype.getDistance = function (another) {
+    Point.prototype.getX = function () {
+        return this.x;
     };
     return Point;
 }());
 var obj = new Point(10, 20);
 //obj.x=10;
 //obj.y=20;
+var x = obj.getX();
+console.log(x);
 obj.draw();
 //X: 10, Y:20
--- Typescript-learn/class.ts
+++ Typescript-learn/class.ts
@@ -16,15 +16,24 @@ class Point{
 
     }
 
-    getDistance(another : Point)
-    {
 
+    getX()
+    {
+        return this.x;
     }
+
+
+    // getDistance(another : Point)
+    // {
+
+    // }
 }
 
 let obj=new Point(10,20);
 //obj.x=10;
 //obj.y=20;
+let x=obj.getX();
+console.log(x);
 obj.draw();
 
 //X: 10, Y:20
