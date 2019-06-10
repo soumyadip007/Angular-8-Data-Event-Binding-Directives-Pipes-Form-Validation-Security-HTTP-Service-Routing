d73953874df2b37b8572043d96adbc50d3c7244d
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 23:43:00 +0530

    Angular Method & Compoenents

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses' ,     // <div class="courses"> --->   .courses  & # for id
-    template:'<h2>{{title}}<h2>'
+    template:'<h2>{{getTitle()+ " or "+ title}} <h2>'
 })
 export class CourseComponent{
 
     title="List of courses"
+
+    
+    getTitle(){
+
+        return this.title;
+    }
 }
