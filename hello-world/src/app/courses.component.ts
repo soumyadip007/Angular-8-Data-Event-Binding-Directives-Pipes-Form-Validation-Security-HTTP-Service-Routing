e8e661654b884e0b405b02806686284d4e532fc2
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 23:32:43 +0530

    Component BUG fixed

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses' ,     // <div class="courses"> --->   .courses  & # for id
-    template:'<h2>Courses<h2>'
+    template:'<h2>Welcome to Angular Course<h2>'
 })
 export class CourseComponent{
 
