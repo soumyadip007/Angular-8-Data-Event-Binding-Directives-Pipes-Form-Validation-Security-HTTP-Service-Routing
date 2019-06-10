a48ffaef630184e67be8540d56248464c7ceb46c
soumyadip007 <soumyadip.note@gmail.com>
2019-06-11 00:25:45 +0530

    ForLoop with List in Angular

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses' ,     // <div class="courses"> --->   .courses  & # for id
-    template:'<h2>{{getTitle()+ " or "+ title}} <h2>'
+    template: ' <h2>{{getTitle() }} or  {{title}} </h2> <ul>   <li *ngFor="let x of courses">{{x}} </li></ul>'
 })
 export class CourseComponent{
 
     title="List of courses"
 
-    
+    courses=["C","JAVA","CPPs"]
     getTitle(){
 
-        return this.title;
+        return this.title
     }
 }
