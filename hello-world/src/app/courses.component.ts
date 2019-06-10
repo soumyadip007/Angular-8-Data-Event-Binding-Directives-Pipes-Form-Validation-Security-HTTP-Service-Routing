668b5322256717745dd6c2aceb5f630196efa382
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 23:39:16 +0530

    Register course as html component

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses' ,     // <div class="courses"> --->   .courses  & # for id
-    template:'<h2>Welcome to Angular Course<h2>'
+    template:'<h2>{{title}}<h2>'
 })
 export class CourseComponent{
 
+    title="List of courses"
 }
