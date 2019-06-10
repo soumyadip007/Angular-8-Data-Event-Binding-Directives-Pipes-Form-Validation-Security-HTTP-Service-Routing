5187f50a6ac623c5011c522c888ab0f5f1332035
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 23:31:00 +0530

    Angular Component impl (BUG)

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'courses' ,     // <div class="courses"> --->   .courses  & # for id
+    template:'<h2>Courses<h2>'
+})
+export class CourseComponent{
+
+}
