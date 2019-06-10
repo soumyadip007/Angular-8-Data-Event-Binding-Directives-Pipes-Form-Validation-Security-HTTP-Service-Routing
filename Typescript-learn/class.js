01fce4a19c472738dad5600a30f789c29d2a0761
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 21:08:16 +0530

    Class implementation

---
+++ Typescript-learn/class.js
@@ -0,0 +1,10 @@
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+    };
+    return Point;
+}());
+var getDistance = function (pointA, pointB) {
+    // console.log(pointAA.y);
+};
---
+++ Typescript-learn/class.ts
@@ -0,0 +1,18 @@
+class Point{
+    x: number;
+    y: number;
+
+    draw(){
+
+    }
+
+    getDistance(another : Point)
+    {
+        
+    }
+}
+
+let getDistance=(pointA : Point, pointB : Point) =>{
+
+   // console.log(pointAA.y);
+}
