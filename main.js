6bdf59e77ca9d57c3bb80b7fa925d26a84ab744f
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 20:04:03 +0530

    Types in Typescript

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-var count = 5;
-count = 'a';
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var bg = Color.Red;
 /*
+
+let count=5;
+count='a';
+
+
 function doSomething()
 {
     for(let i=0;i<19;i++)
--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
+let a: number;
+let b: boolean;
+let c:string;
+let d:any;
+let e: number[]= [1, 2,3 ];
+let f: any[]=[1, true, 'a', false];
+
+
+const ColorRed=0;
+const ColorGreen=1;
+
+
+enum Color{Red=0, Green=1, Blue};
+let bg=Color.Red;
+
+
+/*
+
 let count=5;
 count='a';
 
 
-/*
 function doSomething()
 {    
     for(let i=0;i<19;i++)
