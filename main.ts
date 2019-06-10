404f075dfac7875e89cd2c7d6b819dd3fb3bb165
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 19:48:15 +0530

    Deploying Basic of typescript

---
+++ main.ts
@@ -0,0 +1,27 @@
+
+
+
+/*
+function doSomething()
+{    
+    for(let i=0;i<19;i++)
+    {
+        console.log('Finally: '+i);
+    }
+
+ //   console.log('Finally: ');
+
+}
+
+doSomething();
+
+
+
+/*function func(message)
+{
+    console.log(message);
+}
+
+var msg="Hello World";
+
+func(msg);*/
