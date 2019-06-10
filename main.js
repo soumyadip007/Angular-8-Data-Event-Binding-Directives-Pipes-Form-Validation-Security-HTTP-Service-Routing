3d1db7318ec1c5c5a4a0faddb58523fca4095fbe
soumyadip007 <soumyadip.note@gmail.com>
2019-06-10 20:15:42 +0530

    Typecasting

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-var a;
-var b;
-var c;
-var d;
-var e = [1, 2, 3];
-var f = [1, true, 'a', false];
-var ColorRed = 0;
-var ColorGreen = 1;
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-;
-var bg = Color.Red;
 /*
+4 Typecasting
+let message;
+message='abc';
+
+let endWithC= message.endsWith('c');
+let endWithC1=(<string>message).endsWith('c');
+let endWithC2= (message as string).endsWith('c');
+
+console.log(endWithC+" ")
+
+/*
+3. Type
+
+let a: number;
+let b: boolean;
+let c:string;
+let d:any;
+let e: number[]= [1, 2,3 ];
+let f: any[]=[1, true, 'a', false];
+
+
+const ColorRed=0;
+const ColorGreen=1;
+
+
+enum Color{Red=0, Green=1, Blue=3};
+let bg=Color.Red;
+
+
+/*
+2.
 
 let count=5;
 count='a';
@@ -35,7 +50,10 @@ doSomething();
 
 
 
-/*function func(message)
+/*
+
+1.
+function func(message)
 {
     console.log(message);
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+/*
+4 Typecasting
+let message;
+message='abc';
+
+let endWithC= message.endsWith('c');
+let endWithC1=(<string>message).endsWith('c');
+let endWithC2= (message as string).endsWith('c');
+
+console.log(endWithC+" ")
+
+/*
+3. Type
+
 let a: number;
 let b: boolean;
 let c:string;
@@ -10,11 +24,12 @@ const ColorRed=0;
 const ColorGreen=1;
 
 
-enum Color{Red=0, Green=1, Blue};
+enum Color{Red=0, Green=1, Blue=3};
 let bg=Color.Red;
 
 
 /*
+2.
 
 let count=5;
 count='a';
@@ -35,7 +50,10 @@ doSomething();
 
 
 
-/*function func(message)
+/*
+
+1.
+function func(message)
 {
     console.log(message);
 }
