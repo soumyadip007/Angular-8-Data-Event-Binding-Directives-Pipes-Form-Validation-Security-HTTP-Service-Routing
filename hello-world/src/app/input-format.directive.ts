78a63cedae70a341054d85243792b9da3425d7a6
soumyadip007 <soumyadip.note@gmail.com>
2019-06-12 01:15:09 +0530

    Custom  Directive

--- hello-world/src/app/input-format.directive.ts
+++ hello-world/src/app/input-format.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive,HostListener, ElementRef, Input } from '@angular/core';
+
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
-  constructor() { }
+  @Input('appInputFormat') format;
+
+  constructor(private el:ElementRef) { }
+  // @HostListener('focus') onFocus(){
+  //   console.log("On Focus");
+  // }
+
+  @HostListener('blur') onBlur(){
+   // console.log("On Blur");
+    let val=this.el.nativeElement.val;
+    if(this.format == 'lowercase')
+      this.el.nativeElement.val=val.toLowerCase();
+    else
+    this.el.nativeElement.val=val+val;
+  }
+
+
 
 }
