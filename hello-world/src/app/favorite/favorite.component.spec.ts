03c388759f15bcaa224c9630cd9d942cd2bd58ad
soumyadip007 <soumyadip.note@gmail.com>
2019-06-11 15:32:14 +0530

    Favorite config

--- hello-world/src/app/favorite/favorite.component.spec.ts
+++ hello-world/src/app/favorite/favorite.component.spec.ts
@@ -19,7 +19,7 @@ describe('FavoriteComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-favorite',
+  selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
+  isFavorite: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick() { 
+    this.isFavorite = !this.isFavorite;
+  }
 }
