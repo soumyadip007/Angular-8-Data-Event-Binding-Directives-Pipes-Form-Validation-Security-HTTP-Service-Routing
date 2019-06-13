a14b09ac36b237d12bf44087c8919953ee63dae6
soumyadip007 <soumyadip.note@gmail.com>
2019-06-13 23:25:19 +0530

    ReactiveFormsModule imported

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms' ;
+import { FormsModule, ReactiveFormsModule } from '@angular/forms' ;
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -26,7 +26,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ CoursesService],
   bootstrap: [AppComponent]
