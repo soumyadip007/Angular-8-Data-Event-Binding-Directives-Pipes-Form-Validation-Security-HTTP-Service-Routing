8d6febe1b916ba16308c1aa52aef5b46234af2d8
soumyadip007 <soumyadip.note@gmail.com>
2019-06-13 23:57:16 +0530

    cannotContainSpace validator added

--- hello-world/src/app/signup-form/username.validator.ts
+++ hello-world/src/app/signup-form/username.validator.ts
@@ -1,3 +1,12 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
 export class UsernameValidators{
-    
+
+    cannotContainSpace( control: AbstractControl) : ValidationErrors{
+
+        if((control.value as string).indexOf(' ')>= 0)
+        {
+            return {cannotContainSpace: true}
+        }
+    }
 }
