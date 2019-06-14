d81d8c1ec687391c4d4565149a3b3f6ec97c2fb9
soumyadip007 <soumyadip.note@gmail.com>
2019-06-14 23:50:21 +0530

    All Bug Fixed Yaaaaaaaaaaaaaaaahhhhhhhhhh--HOOO

--- hello-world/src/app/posts/posts.component.spec.ts
+++ hello-world/src/app/posts/posts.component.spec.ts
@@ -19,7 +19,7 @@ describe('PostsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
