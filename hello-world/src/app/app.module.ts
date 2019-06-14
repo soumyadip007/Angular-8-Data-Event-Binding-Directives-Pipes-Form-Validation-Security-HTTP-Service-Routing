d288f1c2a04589b7aab84fba010879403849edea
soumyadip007 <soumyadip.note@gmail.com>
2019-06-14 12:14:13 +0530

    PostComponent Added to App.module.ts

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ContactFormComponent } from "./contact-form/contact-form.component";
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
    InputFormatDirective,
    ContactFormComponent,
    SignupFormComponent,
+   PostsComponent,
   ],
   imports: [
     BrowserModule,
