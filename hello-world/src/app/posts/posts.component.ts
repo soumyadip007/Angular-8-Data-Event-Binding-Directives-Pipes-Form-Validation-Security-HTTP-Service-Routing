04bc74baaf7e957c4d66229b1d92994c4de3cf45
soumyadip007 <soumyadip.note@gmail.com>
2019-06-14 23:47:39 +0530

    post.component updated

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -1,4 +1,61 @@
+import { NotFoundError } from './../common/not-found-error';
+import { BadInput } from './../common/bad-input';
+import { AppError } from './../common/app-error';
+import { PostService } from './../services/post.service';
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts: any[];
+
+  constructor(private service: PostService) {
+  }
+
+  ngOnInit() {
+    this.service.getAll()
+      .subscribe(posts => this.posts = posts);
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    this.posts.splice(0, 0, post);
+
+    input.value = '';
+
+    this.service.create(post)
+      .subscribe(
+        newPost => {
+          post['id'] = newPost.id;
+        },
+        (error: AppError) => {
+          this.posts.splice(0, 1);
+
+          if (error instanceof BadInput) {
+            // this.form.setErrors(error.originalError);
+          }
+          else throw error;
+        });
+  }
+
+  updatePost(post) {
+    this.service.update(post)
+      .subscribe(
+        updatedPost => {
+          console.log(updatedPost);
+        });
+  }
+
+  deletePost(post) {
+    this.service.delete(post.id);
+  }
+}
+
+
+/*
 //import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
 import { PostService } from './../services/post.service';
@@ -89,3 +146,4 @@ export class PostsComponent implements OnInit  {
 
 
 }
+*/
