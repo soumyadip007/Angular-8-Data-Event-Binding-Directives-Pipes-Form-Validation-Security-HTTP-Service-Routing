e55e4f42c9c5c829091c9974b815332c53ff350e
soumyadip007 <soumyadip.note@gmail.com>
2019-06-14 23:16:00 +0530

    useleass compnent removed #########router

--- route-github/src/app/app.module.ts
+++ route-github/src/app/app.module.ts
@@ -4,11 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule} from '@angular/http';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
-import { DataService } from './services/data.service';
-import { AppError } from './common/app-error';
-import { BadInput } from './common/bad-input';
-import { NotFoundError } from './common/not-found-error';
-import { custom } from 'src/app/custom.pipe'
 import { GithubFollowersService } from './services/github-followers.service';
 
 
@@ -17,7 +12,6 @@ import { GithubFollowersService } from './services/github-followers.service';
   declarations: [
     AppComponent,
     GithubFollowersComponent,
-    custom,
   ],
  
   imports: [
