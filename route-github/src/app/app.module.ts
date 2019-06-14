6b902783d0441166b0c2fbee1d43408c9edb4424
soumyadip007 <soumyadip.note@gmail.com>
2019-06-14 22:50:18 +0530

    Some module Removed from route

--- route-github/src/app/app.module.ts
+++ route-github/src/app/app.module.ts
@@ -4,19 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule} from '@angular/http';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
-import { GithubFollowersService } from './services/github-followers.service';
 import { DataService } from './services/data.service';
 import { AppError } from './common/app-error';
 import { BadInput } from './common/bad-input';
 import { NotFoundError } from './common/not-found-error';
 import { custom } from 'src/app/custom.pipe'
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     GithubFollowersComponent,
-    GithubFollowersService,
-    DataService,
     AppError,
     BadInput,
     NotFoundError,
