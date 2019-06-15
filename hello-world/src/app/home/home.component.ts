ca8d16a579d873162339c2667575de9f795149d2
soumyadip007 <soumyadip.note@gmail.com>
2019-06-15 16:53:35 +0530

    home.component.ts config

--- hello-world/src/app/home/home.component.ts
+++ hello-world/src/app/home/home.component.ts
@@ -5,11 +5,50 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  viewMode="map";
 
-  ngOnInit() {
-  }
+  // course=[
+  //   {id:1, name: 'C'},
+  //   {id:2, name: 'Java'},
+  //   {id:3, name: 'CPP'}
+  // ];
+
+  course;
+  
+  post={
+  title : 'Angular App',
+  isFavorite: true
+}
+
+onAdd()
+{
+  this.course.push( {id: 4, name: 'JS'});
+}
+
+onRemove(course)
+{
+  let index=this.course.indexOf( course);
+  this.course.splice(index,1);
+//  course.name="Updated";
+
+}
+
+
+loadCourses()
+{
+this.course=[
+  {id:1, name: 'C'},
+  {id:2, name: 'Java'},
+  {id:3, name: 'CPP'}
+];
+}
+
+task={
+title: 'Review Application',
+assignee: 'Hey'
+}
+
 
 }
