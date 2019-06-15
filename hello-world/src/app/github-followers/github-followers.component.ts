184d12e3cdf936de7332fdb66929243d4098a0dd
soumyadip007 <soumyadip.note@gmail.com>
2019-06-15 20:16:09 +0530

    map & switchmap import

--- hello-world/src/app/github-followers/github-followers.component.ts
+++ hello-world/src/app/github-followers/github-followers.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable  } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'github-followers',
