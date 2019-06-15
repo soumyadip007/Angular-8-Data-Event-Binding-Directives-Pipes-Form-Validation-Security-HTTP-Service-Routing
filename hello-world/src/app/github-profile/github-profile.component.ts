a3c4e4c9ed51e9ae69fa18bfb0cacd55537a1da5
soumyadip007 <soumyadip.note@gmail.com>
2019-06-15 20:21:13 +0530

    git prof method rm

--- hello-world/src/app/github-profile/github-profile.component.ts
+++ hello-world/src/app/github-profile/github-profile.component.ts
@@ -6,20 +6,18 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent {
 
   constructor( private route: ActivatedRoute) { }
 
-  ngOnInit() {
-   this.route.paramMap.subscribe(params =>{});
+  // ngOnInit() {
+  //  this.route.paramMap.subscribe(params =>{});
    
-    // this.route.paramMap
-    // .subscribe(params =>{
-    //  let id= +params.get('id');
-    //   console.log(id);
-    // });
-
-
-  }
+  //   // this.route.paramMap
+  //   // .subscribe(params =>{
+  //   //  let id= +params.get('id');
+  //   //   console.log(id);
+  //   // });
+  // }
 
 }
